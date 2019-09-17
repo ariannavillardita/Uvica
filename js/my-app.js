09426863f78b87b570e3b5c69a777492db0f28d8
ariannavillardita <51307697+ariannavillardita@users.noreply.github.com>
2019-09-16 18:27:41 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,58 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/perfil',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+		{
+		path: '/eventos/',
+    	url: 'eventos.html',
+    	name: 'eventos',
+  		},
+		{
+		path: '/Afiliados/',
+    	url: 'afiliados.html',
+    	name: 'Afiliados',
+  		},
+		{
+		path: '/uvicanights/',
+    	url: 'uvicanights.html',
+    	name: 'Uvica Nights',
+  		}
+		{
+		path: '/team/',
+    	url: 'team.html',
+    	name: 'Team',
+  		}
+		
+		
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
